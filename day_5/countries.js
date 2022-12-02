45080ad0ab92fd8631f136eeea2022e41f470341
Tomi <mariamismael904@gmail.com>
2022-12-03 00:02:03 +0100

    added solutions to unsolved exercises

--- day_5/countries.js
+++ day_5/countries.js
@@ -11,3 +11,20 @@
     'Japan',
     'Kenya'
   ];
+
+  let sliceCountries = countries.slice(0,10);
+  // console.log(sliceCountries)
+
+  //num 2
+  // let middleCountry = countries[countries.length >> 1] method 1; use of bitshifting
+
+let middleCountry = countries[Math.floor(countries.length / 2)]
+  // console.log(middleCountry)
+
+  //num 3
+  let divideCountries = []
+let partaCountries = []
+let partbCountries = []
+
+// console.log(partaCountries)
+// console.log(partbCountries)
--- day_5/level_3.js
+++ day_5/level_3.js
@@ -1,4 +1,40 @@
 //num 1
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
-let sort = ages.sort();
-console.log(sort);
+
+//i
+let sortAges = ages.sort(function(a, b) {
+    return a - b
+      })
+console.log(sortAges);
+
+//ii
+let minAge = sortAges[0];
+// console.log(minAge)
+
+//iii
+let maxAge = ages[sortAges.length - 1];
+// console.log(maxAge);
+
+//iv
+const medianAge = ages[Math.floor(sortAges.length / 2)]
+// console.log(medianAge)
+
+//v
+let sumAge = sortAges.reduce(function(curr, prev) {
+    return curr + prev
+})
+// console.log(sumAge)
+
+let averageAge = sumAge / sortAges.length 
+
+// console.log(averageAge)
+
+//vi
+let agesRange = maxAge- minAge
+// console.log(agesRange)
+
+//vii
+let valCompare_a = Math.abs(minAge - averageAge);
+let valCompare_b = Math.abs(maxAge - averageAge);
+console.log(valCompare_a)
+console.log(valCompare_b)
