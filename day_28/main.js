54eecfe36a350726405d078edfce990894d50d44
Tomi <mariamismael904@gmail.com>
2023-01-12 00:50:07 +0100

    completed challenge leaderboard

--- day_28/main.js
+++ day_28/main.js
@@ -1,116 +1,111 @@
-
-
-const inputDiv = document.querySelector('.input')
-const firstName =  document.querySelector('.input_text')
-const lastName =  document.querySelector('.text_1')
-const country =  document.querySelector('.text_2')
-const score =  document.querySelector('.text_3')
+const section = document.querySelector("section");
+let f_name = document.querySelector('.f_name')
+let l_name = document.querySelector('.l_name')
+let countryInput = document.querySelector('.country')
+let scoreInput = document.querySelector('.score')
 const add_btn = document.querySelector('.add_btn')
-const errorMessage = document.querySelector('.error_message')
-errorMessage.textContent = ``
+const error_message = document.querySelector(".error_message");
+  
 
-firstName.addEventListener('input', () => {
+let getDate = new Date();
+let month = () => {
+  let month = getDate.getMonth();
+  if (month == 0) {
+    month = "JAN";
+  } else if (month == 1) {
+    month = "FEB";
+  } else if (month == 2) {
+    month = "MAR";
+  } else if (month == 3) {
+    month = "APR";
+  } else if (month == 4) {
+    month = "MAY";
+  } else if (month == 5) {
+    month = "JUN";
+  } else if (month == 6) {
+    month = "JUL";
+  } else if (month == 7) {
+    month = "AUG";
+  } else if (month == 8) {
+    month = "SEPT";
+  } else if (month == 9) {
+    month = "OCT";
+  } else if (month == 10) {
+    month = "NOV";
+  } else month = "DEC";
 
-})
-let alphabets = 'A, B, C, D'
+  return month;
+};
+
+
+
+
+const add = () => {
+  const add = document.createElement("li");
+  section.appendChild(add);
+  add.className = 'add'
+  const nameBox = document.createElement("div");
+  add.appendChild(nameBox);
+  const name = document.createElement("h5");
+  const date = document.createElement("p");
+  nameBox.appendChild(name);
+  nameBox.appendChild(date);
+  name.textContent = `${f_name.value} ${l_name.value}`;
+  date.className = 'date'
+  date.textContent = `${month()}, ${getDate.getDate()}, ${getDate.getFullYear()} ${
+    getDate.getHours() + 1
+  }:${getDate.getMinutes()}`
+
+  const country = document.createElement("h5")
+  country.className = 'top'
+  add.appendChild(country)
+  country.textContent = countryInput.value
+
+  let score = document.createElement("h5")
+  add.appendChild(score);
+  score.textContent = scoreInput.value;
+  score.className = 'top'
+
+  let icons = document.createElement("div")
+  add.appendChild(icons);
+  icons.className = 'icons'
+  let delBtn = document.createElement("div")
+  delBtn.className = 'delBtn'
+  delBtn.textContent = 'del'
+  
+  icons.appendChild(delBtn);
+
+  let plus = document.createElement("div")
+  plus.className = 'plus'
+  plus.textContent = "+5"
+  let minus = document.createElement("div")
+  minus.className = 'minus'
+  minus.textContent = "-5";
+  icons.appendChild(plus);
+  icons.appendChild(minus);
+  delBtn.addEventListener("click", (e) => {
+    add.parentNode.removeChild(add)
+  });
+}
 
 add_btn.addEventListener('click', () => {
-if(firstName.value === '' || lastName.value === '' || country.value === '' || score.value === '') {
-    errorMessage.textContent = `All fields are required`
-}  else if(firstName.value === String || lastName.value === String || country.value === String || score.value === Number) {
-outputDiv.append(alphabets)
-}
 
-else {
-    errorMessage.textContent = ''
-}
-
+    if (
+        f_name.value === '' ||
+        l_name.value === '' ||
+        countryInput.value === '' ||
+        scoreInput.value === ''
+      ) {
+             error_message.textContent = "All fields are required"
+      } else {
+        error_message.textContent = ''
+        add()
+      }
+    
+      f_name.value = ''
+      l_name.value = ''
+      countryInput.value = ''
+      scoreInput.value = ''
+    
 })
 
-const delButton = document.querySelector('.del_btn')
-delButton.addEventListener('click', () => {
-    console.log('blu')
-})
-
-const delButton_1 = document.querySelector('.del_btn_1')
-delButton_1.addEventListener('click', () => {
-    console.log('clicked')
-})
-
-const players = [
-    {
-        'name': 'MARTHA YOHANES',
-        'country': 'FINLAND',
-        'score': 85,
-        'icon': delButton,
-        'add': +5,
-        'subtract': -5
-    },
-    {
-        'name': 'DAVID SMITH',
-        'country': 'UNITED KINGDOM',
-        'score': 80,
-        'icon': delButton_1,
-        'add': +5,
-        'subtract': -5
-    },
-    {
-        'name': 'ASABENEH YETAYEH',
-        'country': 'FINLAND',
-        'score': 75,
-        'icon': 'del',
-        'add': +5,
-        'subtract': -5
-    },
-    {
-        'name': 'MATHIAS ELIAS',
-        'country': 'SWEDEN',
-        'score': 70,
-        'icon': 'del',
-        'add': +5,
-        'subtract': -5
-    }
-]
-
-
-let countries
-let names
-let scores
-let del
-let add
-let sub
-let space = ' '
-
-for(const item of players) {
-const outputDiv = document.createElement('div')
-const nameOutputDiv = document.createElement('p')
-nameOutputDiv.className = 'name'
-const countryOutputDiv = document.createElement('p')
-countryOutputDiv.className = 'country'
-const scoreOutputDiv = document.createElement('p')
-scoreOutputDiv.className = 'score'
-const all = document.createElement('div')
-all.className = 'all'
-const delOutputDiv = document.createElement('p')
-delOutputDiv.className = 'del'
-const addOutputDiv = document.createElement('p')
-addOutputDiv.className = 'add'
-const subOutputDiv = document.createElement('p')
-subOutputDiv.className = 'sub'
-outputDiv.className = 'output_div'
-names = item.name   
-countries = item.country
-scores = item.score
-del = item.icon
-add = item.add
-sub = item.subtract
-nameOutputDiv.append(names)
-countryOutputDiv.append(countries)
-scoreOutputDiv.append(scores)
-delOutputDiv.append(del)
-addOutputDiv.append(add)
-subOutputDiv.append(sub)
-all.append( delOutputDiv, addOutputDiv, subOutputDiv)
-outputDiv.append(nameOutputDiv, countryOutputDiv, scoreOutputDiv, all)
-document.body.append(outputDiv)
-}
