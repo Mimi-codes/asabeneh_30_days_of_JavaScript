85d3ec5520af574fe6a01b095e5c08aec1a0b795
Tomi <mariamismael904@gmail.com>
2023-01-12 15:07:27 +0100

    did the color generator for the text

---
+++ day_29/main.js
@@ -0,0 +1,54 @@
+const section = document.querySelector('section')
+const output =  document.createElement('div')
+output.className = 'output'
+section.append(output)
+
+const text = [
+{
+    'para': 30
+},
+{
+    'para': 'DAYS'
+},
+{
+    'para': 'OF'
+},
+{
+    'para': 'JAVASCRIPT'
+},
+{
+    'para': 'CHALLENGE'
+},
+{
+    'para': 2020
+},
+{
+    'para': 'ASABENEH'
+},
+{
+    'para': 'YETAYEH'
+}
+]
+
+
+function displayOutput() {
+for (const item of text) {
+    let p = document.createElement('p')
+    p.className = 'paragraph'
+    p.innerHTML = item['para']
+   
+
+    const chameleon = () => {
+        let random1 = Math.floor(Math.random() * 255)
+        let random2 = Math.floor(Math.random() * 255)
+        let random3 = Math.floor(Math.random() * 255)
+        let color = `rgb(${random1},${random2},${random3})`
+        p.style.color = color;
+        p.style.fontFamily = '"Rowdies", cursive'
+    }
+    setInterval(chameleon, 1000)
+    output.append(p)
+}
+
+}
+displayOutput()
